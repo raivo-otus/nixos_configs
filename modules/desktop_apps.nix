abb5fd4487d26fc701d4193312f0c5fbaf7d3fec
raivo-otus <hindstrom.rasmus@gmail.com>
2026-03-28 13:56:46 +0200

    delete docker

--- modules/desktop_apps.nix
+++ modules/desktop_apps.nix
@@ -24,12 +24,4 @@
 
   # Enable flatpak
   services.flatpak.enable = true;
-
-  # Docker
-  virtualisation.docker.enable = true;
-  virtualisation.docker.daemon.settings = {
-    data-root = "/home/rph/boxship";
-  };
-
-  users.users.rph.extraGroups = ["docker"];
 }
