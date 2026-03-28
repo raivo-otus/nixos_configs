54c5dad9ad450cca7c9e43ec2703fbe7bdcca9b9
raivo-otus <hindstrom.rasmus@gmail.com>
2026-03-28 14:13:54 +0200

    add cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Configuration flake for Desktop-Grenth";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel";
