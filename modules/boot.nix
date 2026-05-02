9aece1d71622867b5b62da2a37ca3c49f7203d7a
raivo-otus <hindstrom.rasmus@gmail.com>
2026-05-02 14:22:19 +0300

    boot mod

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,10 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 0; # boots immediately; press Space to open the menu
 
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "bgrt"; # OEM logo + spinner; change to "spinner" if no OEM logo
+  boot.plymouth = {
+    enable = true;
+    theme = "nixos-bgrt";
+    themePackages = [pkgs.nixos-bgrt-plymouth];
+  };
 
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
