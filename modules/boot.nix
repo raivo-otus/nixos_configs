8e91e9e3872275d7aa8a1d4b25a04e889a64ace5
raivo-otus <hindstrom.rasmus@gmail.com>
2026-05-02 14:14:48 +0300

    plymouth

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,4 +1,17 @@
 {...}: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0; # boots immediately; press Space to open the menu
+
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "bgrt"; # OEM logo + spinner; change to "spinner" if no OEM logo
+
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "rd.udev.log_level=3"
+    "udev.log_level=3"
+  ];
 }
