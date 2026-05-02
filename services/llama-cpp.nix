b26f4be7242dcf187077fa48da4c696f5b09ebbf
raivo-otus <hindstrom.rasmus@gmail.com>
2026-05-02 15:02:37 +0300

    llama fix

--- services/llama-cpp.nix
+++ services/llama-cpp.nix
@@ -1,24 +1,26 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   services.llama-cpp = {
     enable = true;
-    package = pkgs.llama-cpp.override { rocmSupport = true; };
+    package = pkgs.llama-cpp.override {rocmSupport = true;};
     host = "0.0.0.0";
     port = 8081;
     # Place your GGUF model at this path before starting the service
-    model = "/srv/Hoard/models/model.gguf";
+    model = "/srv/Hoard/Models/Qwen3.5-9B-UD-Q6_K_XL.gguf";
     extraFlags = [
-      "--n-gpu-layers" "999"  # offload all layers to the 7900 GRE
-      "--ctx-size" "8192"
-      "--parallel" "2"
-      "--alias" "local"
+      "--n-gpu-layers"
+      "999" # offload all layers to the 7900 GRE
+      "--ctx-size"
+      "8192"
+      "--parallel"
+      "2"
+      "--alias"
+      "local"
     ];
   };
 
   systemd.tmpfiles.rules = [
-    "d /srv/Hoard/models 0755 root root -"
+    "d /srv/Hoard/Models 0755 root root -"
   ];
 
-  networking.firewall.allowedTCPPorts = [ 8081 ];
+  networking.firewall.allowedTCPPorts = [8081];
 }
