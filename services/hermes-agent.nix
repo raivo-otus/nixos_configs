dae69dedc3a7a250146b145a76c74635fed11863
raivo-otus <hindstrom.rasmus@gmail.com>
2026-05-04 19:01:56 +0300

    settings

--- services/hermes-agent.nix
+++ services/hermes-agent.nix
@@ -12,10 +12,13 @@
       ];
       extraOptions = ["--device" "/dev/kfd" "--device" "/dev/dri"];
     };
-    settings.model = {
-      # host.docker.internal doesn't exist on Linux; use Docker bridge gateway
-      base_url = "http://172.17.0.1:8081/v1";
-      default = "local";
+    settings = {
+      model = {
+        # host.docker.internal doesn't exist on Linux; use Docker bridge gateway
+        base_url = "http://172.17.0.1:8081/v1";
+        default = "local";
+      };
+      toolsets = ["all"];
     };
   };
 }
